a7c36a6f3d94e81972269c9771679ae390c23967
Thishan Herath <diniduthishan9@gmail.com>
2026-07-25 10:20:57 +0530

    initial project

---
+++ src/main/java/com/ictec/eventmanagementsytem/EventManagementSytemApplication.java
@@ -0,0 +1,13 @@
+package com.ictec.eventmanagementsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementSytemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementSytemApplication.class, args);
+    }
+
+}
